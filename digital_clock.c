543c17de2b9f4e20209eba8afd8091b62afc719b
himanshu <117630839+himanshu18pro@users.noreply.github.com>
2023-10-08 13:14:19 +0530

    Update digital_clock.c

--- digital_clock.c
+++ digital_clock.c
@@ -4,35 +4,36 @@
 #include <time.h>
 int main()
 {
-    int hour, minute, second:
-    //int delay = 1000;
+    int hour, minute, second;
+    // int delay = 1000;
     printf("\nEnter the Clock Time (HH:MM:SS) : ");
     scanf("%d%d%d", &hour, &minute, &second);
-    if (hour > 13 || minute > 60 || second > 60)
+    if (hour >= 13 || minute >= 60 || second >= 60)
     {
         printf("\nError in your Time !! ");
         exit(0);
     }
     while (1)
     {
-        second++;
+        system("cls");
+        printf("\n\t\tClock -> %02d : %02d : %02d ", hour, minute, second);
         if (second > 59)
         {
             minute++;
-            second + 0;
+            second = 0;
+            if (minute > 59)
+            {
+                hour++;
+                minute = 0;
+
+                if (hour > 12)
+                {
+                    hour = 0;
+                }
+            }
         }
-        if (minute > 59)
-        {
-            hour++;
-            minute = 0;
-        }
-        if (hour > 12)
-        {
-            hour = 1;
-        }
-        system("cls");
-        printf("\n\t\tClock -> %02d : %02d : %02d ", hour, minute, second);
-        sleep(1000);
+        second++;
+        sleep(1);
     }
     return 0;
 }
